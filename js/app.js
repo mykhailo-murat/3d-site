8b34404d2fdc64d222077a84128d175d7322e96c
WLAMurat <mikhail@thewhitelabelagency.com>
2022-04-08 23:05:22 +0300

    main engine

--- js/app.js
+++ js/app.js
@@ -15,10 +15,10 @@ window.onscroll = function () {
 
     frames.forEach(function (n, i) {
         zVals.push((i * zSpacing) + zSpacing)
-        zVals[i] += delta * -5.5
+        zVals[i] += delta * -6
         let frame = frames[i],
             transform = `translateZ(${zVals[i]}px)`,
-            opacity = zVals[i] < Math.abs(zSpacing) / 1.2 ? 1 : 0
+            opacity = zVals[i] < Math.abs(zSpacing) / 1.4 ? 1 : 0
         frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
     })
 
